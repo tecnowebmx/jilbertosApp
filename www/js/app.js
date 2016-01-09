955aefa13fcf954e9763d2a2b6a2d9826cf4b26e
Gustavo Reyes <gustavo.reyes@aplimovil.mx>
2016-01-08 16:58:40 -0800

    Se manda llamar el file de los controladores

--- www/js/app.js
+++ www/js/app.js
@@ -1,5 +1,5 @@
 (function(){
-var app = angular.module('starter', ['ionic']);
+var app = angular.module('starter', ['ionic', 'starter.controllers']);
 
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
