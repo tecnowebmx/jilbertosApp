d81650e3decc4287484f7ed9b8d9d4248037c919
Gustavo Reyes <gustavo.reyes@aplimovil.mx>
2016-01-08 16:52:42 -0800

    Se creo el estado para el dashboard

--- www/js/app.js
+++ www/js/app.js
@@ -14,4 +14,26 @@ app.run(function($ionicPlatform) {
     }
   });
 });
+
+app.config(function($stateProvider, $urlRouterProvider){
+  $stateProvider
+
+  .state('tab', {
+  url: '/tab',
+  abstract: true,
+  templateUrl: 'templates/tabs.html'
+  })
+
+  .state('tab.dash', {
+    url: '/dash',
+    views: {
+      'tab-dash': {
+        templateUrl: 'templates/tab-dash.html',
+        controller: 'DashCtrl'
+      }
+    }
+  });
+
+  $urlRouterProvider.otherwise('/tab/dash');
+});
 }());
