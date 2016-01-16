1cfaa5a23e6b9c68218272cf2da45d96155fe33d
Gustavo Reyes <gustavo.reyes@aplimovil.mx>
2016-01-15 18:31:30 -0800

    Modifique los estados para asignar correctamente las urls

--- www/js/app.js
+++ www/js/app.js
@@ -15,14 +15,22 @@ app.run(function($ionicPlatform) {
   });
 });
 
-app.config(function($stateProvider, $urlRouterProvider){
+app.config(function($stateProvider, $urlRouterProvider) {
+
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
   $stateProvider
 
-  .state('tab', {
-  url: '/tab',
-  abstract: true,
-  templateUrl: 'templates/tabs.html'
+  // setup an abstract state for the tabs directive
+    .state('tab', {
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tabs.html'
   })
+
+  // Each tab has its own nav history stack:
 
   .state('tab.dash', {
     url: '/dash',
@@ -32,8 +40,59 @@ app.config(function($stateProvider, $urlRouterProvider){
         controller: 'DashCtrl'
       }
     }
+  })
+
+  .state('tab.store', {
+      url: '/store',
+      views: {
+        'tab-store': {
+          templateUrl: 'templates/tab-store.html',
+          
+        }
+      }
+    })
+
+  .state('tab.promo', {
+      url: '/promo',
+      views: {
+        'tab-promo': {
+          templateUrl: 'templates/tab-promo.html',
+          
+        }
+      }
+    })
+
+  .state('tab.chats', {
+      url: '/chats',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/tab-chats.html',
+          controller: 'ChatsCtrl'
+        }
+      }
+    })
+    .state('tab.chat-detail', {
+      url: '/chats/:chatId',
+      views: {
+        'tab-chats': {
+          templateUrl: 'templates/chat-detail.html',
+          controller: 'ChatDetailCtrl'
+        }
+      }
+    })
+
+  .state('tab.account', {
+    url: '/account',
+    views: {
+      'tab-account': {
+        templateUrl: 'templates/tab-account.html',
+        controller: 'AccountCtrl'
+      }
+    }
   });
 
+  // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/tab/dash');
+
 });
 }());
